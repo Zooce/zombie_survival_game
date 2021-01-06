fb531d7f1d3157cadc5a918021532e060bac9e62
Zooce <zooce@hey.com>
2021-01-05 22:08:25 -0700

    added a temp player movement system and played with mouse tracking

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(player_movement.system())
         .run();
 }
 
@@ -12,6 +13,7 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    // draw the temp player sprite
     let texture_handle = asset_server.load("icon.png");
     commands
         .spawn(Camera2dBundle::default())
@@ -19,4 +21,16 @@ fn setup(
             material: materials.add(texture_handle.into()),
             ..Default::default()
         });
+}
+
+fn player_movement(
+    _time: Res<Time>,
+    _keyboard_input: Res<Input<KeyCode>>,
+    cursor_moved_events: Res<Events<CursorMoved>>,
+    mut cursor_moved_events_reader: Local<EventReader<CursorMoved>>,
+) {
+    // note: cursor position is 0,0 in the bottom left corner, while top left corner is W,H
+    if let Some(e) = cursor_moved_events_reader.latest(&cursor_moved_events) {
+        println!("cursor pos: {}", e.position);
+    }
 }

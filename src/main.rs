1a5ae75d1e8871c1d7b5b3466b7fa4a8dc0c03b5
Zooce <zooce@hey.com>
2021-01-20 22:31:49 -0700

    bullet-zombie collisions + break out zombie spawning + common textures

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,39 @@ fn main() {
         .init_resource::<ZombieTimer>()
         .add_event::<ShootEvent>()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
+        .add_startup_stage("insert_resources", SystemStage::single(insert_reusable_resources.system()))
+        .add_startup_stage_after("insert_resources", "setup", SystemStage::single(setup.system()))
         .add_system(player_movement.system())
         .add_system(zombie_movement.system())
         .add_system(check_mouse_events.system())
         .add_system(handle_shoot_events.system())
         .add_system(bullet_movement.system())
         .add_system(bullet_decay.system())
+        .add_system(check_bullet_collisions.system())
         .run();
+}
+
+fn insert_reusable_resources(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handles = TextureHandles {
+        zombie_handle: materials.add(asset_server.load("Enemy.png").into()),
+        bullet_handle: materials.add(asset_server.load("Bullet.png").into()),
+    };
+    commands.insert_resource(texture_handles);
 }
 
 fn setup(
     commands: &mut Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    texture_handles: Res<TextureHandles>,
 ) {
     // draw the temp player sprite
     let background_texture_handle = asset_server.load("test-background.png");
-    let zombie_texture_handle = asset_server.load("Enemy.png");
+
     commands
         .spawn(Camera2dBundle::default())
 
@@ -40,20 +55,23 @@ fn setup(
             ..Default::default()
         })
         .with(Background)
-
-        // zombie sprite
-        .spawn(SpriteBundle {
-            material: materials.add(zombie_texture_handle.into()),
-            transform: Transform::from_translation(Vec3::new(-300.0, 200.0, 0.0)),
-            ..Default::default()
-        })
-        .with(Zombie{ angle: 0.0 })
         ;
 
     spawn_player(commands, asset_server, materials);
+    spawn_zombie(commands, texture_handles);
 }
 
 struct Background;
+
+struct TextureHandles {
+    zombie_handle: Handle<ColorMaterial>,
+    bullet_handle: Handle<ColorMaterial>,
+}
+
+//------------------------------------------------------------------------------ Common
+
+struct ColliderRadius(f32);
+
 
 //------------------------------------------------------------------------------ Player
 struct Player;
@@ -151,6 +169,21 @@ impl Default for ZombieTimer {
     }
 }
 
+fn spawn_zombie(
+    commands: &mut Commands,
+    texture_handles: Res<TextureHandles>,
+) {
+    commands
+        .spawn(SpriteBundle {
+            material: texture_handles.zombie_handle.clone(),
+            transform: Transform::from_translation(Vec3::new(-300.0, 200.0, 0.0)),
+            ..Default::default()
+        })
+        .with(Zombie { angle: 0.0 })
+        .with(ColliderRadius(32.0))
+        ;
+}
+
 use rand::{thread_rng, Rng};
 fn zombie_movement(
     time: Res<Time>,
@@ -206,18 +239,17 @@ fn handle_shoot_events(
     shoot_events: Res<Events<ShootEvent>>,
     mut shoot_events_reader: Local<EventReader<ShootEvent>>,
     commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    texture_handles: Res<TextureHandles>,
 ) {
     if let Some(event) = shoot_events_reader.iter(&shoot_events).next_back() {
-        let bullet_texture = asset_server.load("Bullet.png");
         commands
             .spawn(SpriteBundle {
-                material: materials.add(bullet_texture.into()),
-                transform: event.0.to_owned(),
+                material: texture_handles.bullet_handle.clone(),
+                transform: event.0.clone(),
                 ..Default::default()
             })
             .with(Bullet::default())
+            .with(ColliderRadius(8.0))
             ;
     }
 }
@@ -245,6 +277,30 @@ fn bullet_decay(
     for (entity, mut bullet) in bullet_query.iter_mut() {
         if bullet.decay_timer.tick(time.delta_seconds()).finished() {
             commands.despawn(entity);
+        }
+    }
+}
+
+fn check_bullet_collisions(
+    zombie_query: Query<(&ColliderRadius, &Transform), With<Zombie>>,
+    bullet_query: Query<(&ColliderRadius, &Transform), With<Bullet>>,
+) {
+    // two circles in 2D are colliding iff:
+    //
+    //      r1 + r2  >  sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    //
+    // or even better:
+    //
+    //      (r1 + r2)^2  >  (x2 - x1)^2 + (y2 - y1)^2
+
+    for (br, bt) in bullet_query.iter() {
+        for (zr, zt) in zombie_query.iter() {
+            let max_dist = (br.0 + zr.0).powi(2);
+            let dist = (bt.translation.x - zt.translation.x).powi(2)
+                + (bt.translation.y - zt.translation.y).powi(2);
+            if dist < max_dist {
+                println!("Collision {} {}", bt.translation, zt.translation);
+            }
         }
     }
 }
